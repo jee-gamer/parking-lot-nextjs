3b9cf2e0d3b14c1fc004fedc16fc1dc223281c2f
Jiramate Kedmake <jiramate2548@gmail.com>
2025-04-02 09:59:53 +0700

    Finish parkingLotSchema methods except constructor

--- lib/ParkingLot.ts
+++ lib/ParkingLot.ts
@@ -11,27 +11,6 @@ export default class ParkingLot {
     private NUM_LEVELS: number = 5;
 
     constructor() {
-        this.levels = Array.from({ length: this.NUM_LEVELS }, (_, i) => new Level(i, 10)); // 10 spots per level for now
-    }
-
-    async parkVehicle(vehicle: Vehicle) {
-        for (let i=0; i< this.levels.length; i++) {
-            if (this.levels[i].parkVehicle(vehicle)) {
-
-                await DB.getConnection()
-                await DB.saveParkingLot(this)
-                await DB.saveVehicle(vehicle)
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public getAttributes() {
-        return {
-            id: this.id,
-            levels: this.levels, // Don't know if this is correct, levels can be modified then, but I dont know if making copies is viable
-            NUM_LEVELS: this.NUM_LEVELS,
-        }
+        this.levels = Array.from({length: this.NUM_LEVELS}, (_, i) => new Level(i, 10)); // 10 spots per level for now
     }
 }
--- models/ParkingLot.ts
+++ models/ParkingLot.ts
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 import levelSchema from "@/models/Level";
 import Level from "@/lib/Level"
+import { TVehicle} from "@/models/Vehicle";
+import DatabaseManager from "@/lib/DatabaseManager";
+
+const DB = DatabaseManager.getInstance();
 
 interface IParkingLot {
     levels: Level[];
@@ -14,5 +18,20 @@ const parkingLotSchema = new mongoose.Schema<IParkingLot>({
         required: true,
     },
 })
+
+
+
+parkingLotSchema.methods.parkVehicle = async function (vehicle: TVehicle) {
+    for (let i=0; i< this.levels.length; i++) {
+        if (this.levels[i].parkVehicle(vehicle)) {
+
+            await DB.getConnection()
+            await DB.saveParkingLot(this)
+            await DB.saveVehicle(vehicle)
+            return true;
+        }
+    }
+    return false;
+}
 
 export default mongoose.models.ParkingLot || mongoose.model("ParkingLot", parkingLotSchema);
