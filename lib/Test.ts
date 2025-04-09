151c3b109c372f2d0eff2e8651e4e3a2bbdb10b1
Jiramate Kedmake <jiramate2548@gmail.com>
2025-04-09 08:38:45 +0700

    Update test

--- lib/Test.ts
+++ lib/Test.ts
@@ -1,5 +1,4 @@
 import { Bus } from "@/models/Bus";
-import ParkingLot from "@/models/ParkingLot";
 import DatabaseManager from "@/lib/DatabaseManager";
 import { Motorcycle } from "@/models/Motorcycle";
 import ParkingManager from "@/lib/ParkingManager";
@@ -9,24 +8,14 @@ await DB.getConnection()
 
 const PM = ParkingManager.getInstance();
 
-const parkingLot = await ParkingLot.getOrCreate()
-console.log(parkingLot)
-const parkingLot2 = await ParkingLot.getOrCreate()
-console.log(parkingLot)
+const bus = await Bus.create({ licensePlate: crypto.randomUUID() })
+const motorcycle = await Motorcycle.create({ licensePlate: crypto.randomUUID() })
 
-console.log(parkingLot2 == parkingLot)
+const busStatus = await PM.park(bus.licensePlate)
+const motorcycleStatus = await PM.park(motorcycle.licensePlate)
 
-// const bus = await Bus.create({ licensePlate: crypto.randomUUID() })
-// const motorcycle = await Motorcycle.create({ licensePlate: crypto.randomUUID() })
-//
-//
-//
-// const busStatus = await PM.park(bus.licensePlate)
-// const motorcycleStatus = await PM.park(motorcycle.licensePlate)
-//
-//
-// console.log(busStatus)
-// console.log(motorcycleStatus)
+console.log(busStatus)
+console.log(motorcycleStatus)
 
-// console.log( await bus.clearSpots() )
-// console.log( await motorcycle.clearSpots() )
+console.log( await bus.clearSpots() )
+console.log( await motorcycle.clearSpots() )
